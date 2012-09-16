f660e683bc65f179c1fec0f0d786004f17deb737
uttarasridhar <uttara.26790@gmail.com>
2012-09-15 20:29:23 -0400

    Added code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackChannelApp::Application.config.secret_token = '16479a6b250c9ebd2a7ac777ce91f85724a271e57dd618440e08dc40c44cedc891c8ecc412794dec96a610e34688af8dd1ad6209200ccb4fd08b05a9cd07512e'
